ec7d15848a86ec9eee4ebffd0f79f05de1e83ede
Darkedgeblood <ifajhossen@gmail.com>
2025-01-31 15:04:14 +0600

    Fix missing dependencies

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"]
+  }));  
 app.use(express.json());
 
 // YouTube API Configuration
